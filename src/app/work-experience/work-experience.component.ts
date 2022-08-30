f6c7026abfe2504c2f46deaf3f98844dc6986078
Itzelll <mmitzel2435@gmail.com>
2022-08-30 12:22:12 -0500

    mensaje branch develop #1

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperience : Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    let work1 = {
+      fecha : "2020-2022",
+      ubicacion : "Orizaba, Ver",
+      puesto : "CEO",
+      empresa : "Waves"
+    };
+
+    let work2 = {
+      fecha : "2017-2020",
+      ubicacion : "Ixtac, Ver",
+      puesto : "CEO",
+      empresa : "Kubeet"
+    };
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
+
   }
 
 }
